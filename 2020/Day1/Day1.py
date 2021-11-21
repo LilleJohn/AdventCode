293424199079ce05727e0f366aff484170fc2eb4
Johan <jofr1989@hotmail.com>
2021-11-22 00:42:07 +0100

    Day1

---
+++ 2020/Day1/Day1.py
@@ -0,0 +1,20 @@
+# Part 1
+with open(r'C:\Projekt\Python\AdventCode\2020\Day1\expense_report.txt') as f:
+    lines = f.readlines()
+
+def part_one():
+    for first in lines:
+        for second in lines:
+            if int(first) + int(second) == 2020:
+                return(int(first)*int(second))
+
+def part_two():
+    for first in lines:
+        for second in lines:
+            for third in lines:
+                if int(first) + int(second) + int(third) == 2020:
+                    return(int(first)*int(second)*int(third))
+
+if __name__ == '__main__':
+    print("Part 1:", part_one())
+    print("Part 2:", part_two())
